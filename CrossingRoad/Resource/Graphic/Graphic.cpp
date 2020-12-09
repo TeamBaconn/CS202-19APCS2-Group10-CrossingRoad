bcb46fd85d354836e84c35efe5ee0549505e1882
John <53034855+par14kour2@users.noreply.github.com>
2020-12-09 21:42:35 +0700

    Quick Fix Quick Sort

--- CrossingRoad/Resource/Graphic/Graphic.cpp
+++ CrossingRoad/Resource/Graphic/Graphic.cpp
@@ -24,12 +24,11 @@ void GotoXY(int x, int y) {
 Graphic::Graphic() {
 
 }
-
 void Graphic::qSort(vector<Entity*> values, int low, int high) {
 	if (low < high) {
 		int i = low - 1;
 		for (int j = low; j < high; ++j) {
-			if (values[i]->pos.y > values[j]->pos.y) {
+			if (values[j]->pos.y > values[high]->pos.y) {
 				++i;
 				swap(values[i], values[j]);
 			}
@@ -41,7 +40,7 @@ void Graphic::qSort(vector<Entity*> values, int low, int high) {
 }
 
 vector<Entity*> Graphic::sort(vector<Entity*> values) {
-	qSort(values, 1, values.size() - 1);
+	qSort(values, 0, values.size() - 1);
 	return values;
 }
 
@@ -56,7 +55,7 @@ char** Graphic::getDrawableMap(const Level& level) {
 	char** ingame = level.generateMap();
 	for (int i = 0; i < INGAME_WIDTH; i++)
 		for (int j = 0; j < INGAME_HEIGHT; j++)
-			draw(map, i, j, ingame, i + x, j + y, level);
+			draw(map,i,j,ingame,i+x,j + y,level);
 
 	Level::deleteMap(ingame, level.getWidth());
 	for (int i = 0; i < entities.size(); i++) {
@@ -69,12 +68,12 @@ char** Graphic::getDrawableMap(const Level& level) {
 				//Check its null character ! by default
 				char c = key.key[j][k];
 
-				drawC(map, pos.x + k - x, pos.y + j - y, c);
+				drawC(map, pos.x + k-x, pos.y + j-y, c);
 			}
 		}
 		//For debug
-		drawC(map, entities[i]->pos.x - x, entities[i]->pos.y - y, '0' + i);
-		drawC(map, pos.x - x, pos.y - y, 'X');
+		drawC(map, entities[i]->pos.x-x, entities[i]->pos.y-y, '0' + i);
+		drawC(map, pos.x-x, pos.y-y, 'X');
 	}
 	return map;
 }

5ed6cda73d851d25d4f98177afabb6389646db4e
fizzy-1111 <luchuongtam@gmail.com>
2020-12-11 01:22:57 +0700

    finish trim

--- CrossingRoad/Resource/Level.h
+++ CrossingRoad/Resource/Level.h
@@ -53,29 +53,23 @@ public:
 	void ReplaceAll(string& c, char f, char t) {
 		for (int i = 0; i < c.length(); i++) if (c[i] == f) c[i] = t;
 	}
-	Frame trim(Frame fr) {
+	void trim(Frame& fr) {
 	
 		int mintrim=999;
 		int maxtrim=0;
 		for (int i = 0; i < fr.key.size(); i++) {
-			for (int j = 0; j < fr.key[i].size(); j++) {
-				if (fr.key[i][j] != ' ' && j < mintrim) {
-					mintrim = j;
-					break;
+				for (int j = 0; j < fr.key[i].size(); j++) {
+					if (fr.key[i][j] != '!' && j < mintrim) {
+						mintrim = j;
+						break;
+					}
 				}
-			}
-			for (int j = fr.key[i].size() - 1; j >= 0; j--) {
-				if (fr.key[i][j] != ' ' && j < maxtrim) {
-					maxtrim = j;
-					break;
-				}
-			}
 		}
 		for (int i = 0; i < fr.key.size(); i++) {
-			fr.key[i].erase(0, mintrim - 1);
-			fr.key[i].erase(maxtrim + 1, fr.key[i].size() - 1);
+			if (!fr.key[i].empty()&&mintrim> 0) {
+				fr.key[i].erase(0, mintrim );
+			}
 		}
-		return fr;
 	}
 	Animator* readAnimator(string path, int id) {
 		ifstream info((string)ANIMATION + "/" + path);
@@ -92,13 +86,13 @@ public:
 			ReplaceAll(k, ' ', '!');//Set background opacity to 0
 			if (max == -1 || max < k.length()) max = k.length();
 			if (k == "X") {
+				trim(kc);
 				frame.push_back(kc);
 				kc = *new Frame();
 				continue;
 			}
 			kc.key.push_back(k);
 		}
-		trim(kc);
 		return new Animator(frame, speed, id, max, set);
 	}
 	Level() = default;

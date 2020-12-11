e86de5e91dec98e2d43dbdc295648ae825b2ed5b
fizzy-1111 <luchuongtam@gmail.com>
2020-12-11 13:25:50 +0700

    update rightrim

--- CrossingRoad/Resource/Graphic/Animator.cpp
+++ CrossingRoad/Resource/Graphic/Animator.cpp
@@ -1,27 +1,45 @@
 #include "Animator.h"
 
-Frame trim(Frame fr) {
+void lefttrim(Frame& fr) {
+
 	int mintrim = 999;
-	int maxtrim = 0;
 	for (int i = 0; i < fr.key.size(); i++) {
 		for (int j = 0; j < fr.key[i].size(); j++) {
-			if (fr.key[i][j] != ' ' && j < mintrim) {
+			if (fr.key[i][j] != '!' && j < mintrim) {
 				mintrim = j;
 				break;
 			}
 		}
+	}
+	for (int i = 0; i < fr.key.size(); i++) {
+		if (!fr.key[i].empty() && mintrim > 0) {
+			fr.key[i].erase(0, mintrim);
+		}
+	}
+}
+void righttrim(Frame& fr) {
+	int longest = 0;
+	int maxtrim = 0;
+	for (int j = 0; j < fr.key.size(); j++) {
+		if (fr.key[j].size() > longest) longest = fr.key[j].size();
+	}
+	for (int j = 0; j < fr.key.size(); j++) {
+		while (fr.key[j].size() < longest) fr.key[j] += '!';
+	}
+	for (int i = 0; i < fr.key.size(); i++) {
 		for (int j = fr.key[i].size() - 1; j >= 0; j--) {
-			if (fr.key[i][j] != ' ' && j < maxtrim) {
+			if (fr.key[i][j] != '!' && j > maxtrim) {
 				maxtrim = j;
 				break;
 			}
 		}
 	}
-	for (int i = 0; i < fr.key.size(); i++) {
-		fr.key[i].erase(0, mintrim - 1);
-		fr.key[i].erase(maxtrim + 1, fr.key[i].size() - 1);
+	for (int j = 0; j < fr.key.size(); j++) {
+		if (!fr.key[j].empty() && fr.key[j].size() > maxtrim) {
+			fr.key[j].erase(maxtrim+1 , fr.key[j].size());
+		}
 	}
-	return fr;
+
 }
 
 void ReplaceAll(string& c, char f, char t)
@@ -45,7 +63,8 @@ Animator* readAnimator(string path, int id)
 		ReplaceAll(k, ' ', '!');//Set background opacity to 0
 		if (max == -1 || max < k.length()) max = k.length();
 		if (k == "X") {
-			//trim(kc);
+			lefttrim(kc);
+			righttrim(kc);
 			frame.push_back(kc);
 			kc = *new Frame();
 			continue;
